7945dfe08dfae17d42487c2963dc2678f39276ec
kzryzstof <38137839+kzryzstof@users.noreply.github.com>
2025-05-25 11:01:35 -0400

    Corrections.

--- devops/terraform/resources/container_app_environment.tf
+++ devops/terraform/resources/container_app_environment.tf
@@ -1,6 +1,6 @@
 data "azurerm_container_app_environment" "shared" {
-    resource_group_name        = local.container_app_environment.resource_group_name
-    name                       = local.container_app_environment.name
+    resource_group_name        = var.container_app_environment_resource_group_name
+    name                       = var.container_app_environment_name
     
     provider = azurerm.shared_resources
 }
