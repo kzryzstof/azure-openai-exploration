40f47ed29846c6e3e2533ef68467de125ae78033
kzryzstof <38137839+kzryzstof@users.noreply.github.com>
2025-05-26 08:23:47 -0400

    Correction.

--- devops/terraform/resources/ai_model.tf
+++ devops/terraform/resources/ai_model.tf
@@ -1,6 +1,6 @@
-resource "azurerm_cognitive_deployment" "gpt4o_model" {
+resource "azurerm_cognitive_deployment" "o3_mini_model" {
   cognitive_account_id = azurerm_ai_services.default.id
-  name                 = "gpt4o"
+  name                 = "o3_mini"
   
   model {
     format = "OpenAI"
