d430836b81cce7da4f0c89b911d8d6602ce7ba3f
kzryzstof <38137839+kzryzstof@users.noreply.github.com>
2025-05-29 07:44:02 -0400

    Try deployment another instance.

--- devops/terraform/resources/ai_deployment_speech.tf
+++ devops/terraform/resources/ai_deployment_speech.tf
@@ -1,15 +1,15 @@
-# resource "azurerm_cognitive_deployment" "speech_model" {
-#   cognitive_account_id = azurerm_ai_services.default.id
-#   name                 = "speech-model"
-#   
-#   model {
-#     format  = "OpenAI"
-#     name    = "gpt-4o-mini-tts"
-#     version = "2025-03-20"
-#   }
-#   
-#   sku {
-#     name = "GlobalStandard"
-#     capacity = 10
-#   }
-# }
+resource "azurerm_cognitive_deployment" "speech_model" {
+  cognitive_account_id = azurerm_ai_services.default.id
+  name                 = "speech-model"
+  
+  model {
+    format  = "OpenAI"
+    name    = "gpt-4o-mini-tts"
+    version = "2025-03-20"
+  }
+  
+  sku {
+    name = "GlobalStandard"
+    capacity = 10000
+  }
+}
